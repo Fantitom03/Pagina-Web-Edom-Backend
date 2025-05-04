51c1fa7bbeccd260f317217227f0221f1c49d720
Fantitom03 <leivafrancisco2017@gmail.com>
2025-05-04 04:54:04 -0300

    como antes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { register, login } from '../controllers/authController.js';
 import { registerValidation, loginValidation } from '../validations/authValidations.js';
-import { authenticateToken, authorize } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
